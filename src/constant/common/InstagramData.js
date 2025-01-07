7cbf88ac2a4310731441fb2ab90cb362f1a56ee2
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2025-01-07 17:55:56 +0545

    instagram photo changed

--- src/constant/common/InstagramData.js
+++ src/constant/common/InstagramData.js
@@ -3,23 +3,23 @@ const InstagramData = {
     images:[
         {
             id: '1',
-            img: '/assets/images/instagram/insta-1.webp'
+            img: '/assets/images/instagram/insta-1.png'
         },
         {
             id: '2',
-            img: '/assets/images/instagram/insta-2.webp'
+            img: '/assets/images/instagram/insta-2.png'
         },
         {
             id: '4',
-            img: '/assets/images/instagram/insta-3.webp'
+            img: '/assets/images/instagram/insta-3.png'
         },
         {
             id: '5',
-            img: '/assets/images/instagram/insta-4.webp'
+            img: '/assets/images/instagram/insta-4.png'
         },
         {
             id: '6',
-            img: '/assets/images/instagram/insta-5.webp'
+            img: '/assets/images/instagram/insta-5.png'
         }
     ]
 }
