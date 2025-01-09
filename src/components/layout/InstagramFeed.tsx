20e6ec94ee1062c52a2b8647a3ab932a76d6aacf
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2025-01-09 14:53:31 +0545

    unwanted images cleaning part3

--- src/components/layout/InstagramFeed.tsx
+++ src/components/layout/InstagramFeed.tsx
@@ -6,50 +6,6 @@ import Image from 'next/image';
 const InstagramFeed = () => {
     return (
         <div className="instagram_feed_style__one lg:pt-30 pt-24 ">
-            {/* <div className="swiper max-w-[1570px] mx-auto px-3 insta-feed-slider">
-                <div className="swiper-wrapper ">
-                    <div className="swiper-slide">
-                        <div className="relative overflow-hidden group">
-                            <img src="./assets/images/instagram/insta-1.webp" alt="instagram" className="w-full" />
-                            <a href="#" className="insta-feed-icon">
-                                <i className="bi bi-instagram" />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="swiper-slide">
-                        <div className="relative overflow-hidden group">
-                            <img src="./assets/images/instagram/insta-2.webp" alt="instagram" className="w-full" />
-                            <a href="#" className="insta-feed-icon">
-                                <i className="bi bi-instagram" />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="swiper-slide">
-                        <div className="relative overflow-hidden group">
-                            <img src="./assets/images/instagram/insta-3.webp" alt="instagram" className="w-full" />
-                            <a href="#" className="insta-feed-icon">
-                                <i className="bi bi-instagram" />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="swiper-slide">
-                        <div className="relative overflow-hidden group">
-                            <img src="./assets/images/instagram/insta-4.webp" alt="instagram" className="w-full" />
-                            <a href="#" className="insta-feed-icon">
-                                <i className="bi bi-instagram" />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="swiper-slide">
-                        <div className="relative overflow-hidden group">
-                            <img src="./assets/images/instagram/insta-5.webp" alt="instagram" className="w-full" />
-                            <a href="#" className="insta-feed-icon">
-                                <i className="bi bi-instagram" />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
             <Swiper 
                      slidesPerView={1}
                      spaceBetween={30}
--- src/constant/common/InstagramData.js
+++ src/constant/common/InstagramData.js
@@ -1,26 +1,26 @@
 
 const InstagramData = {
-    images:[
-        {
-            id: '1',
-            img: '/assets/images/instagram/insta-1.png'
-        },
-        {
-            id: '2',
-            img: '/assets/images/instagram/insta-2.png'
-        },
-        {
-            id: '4',
-            img: '/assets/images/instagram/insta-3.png'
-        },
-        {
-            id: '5',
-            img: '/assets/images/instagram/insta-4.png'
-        },
-        {
-            id: '6',
-            img: '/assets/images/instagram/insta-5.png'
-        }
-    ]
-}
+  images: [
+    {
+      id: "1",
+      img: `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}img1.png`,
+    },
+    {
+      id: "2",
+      img: `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}img2.png`,
+    },
+    {
+      id: "3",
+      img: `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}img3.png`,
+    },
+    {
+      id: "4",
+      img: `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}img4.png`,
+    },
+    {
+      id: "5",
+      img: `${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}img5.png`,
+    },
+  ],
+};
 export default InstagramData; 
